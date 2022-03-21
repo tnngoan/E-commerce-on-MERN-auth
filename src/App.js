1590c68d93ce2c4d6670934b217eec44a63ea5c2
tnngoan <ngoan.n.tr@gmail.com>
2022-03-21 19:20:56 +0700

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Navigation />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  )
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
