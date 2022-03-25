06b3d83177ebbbbebc679887cf274d3097785db0
tnngoan <ngoan.n.tr@gmail.com>
2022-03-26 01:41:57 +0700

    add-products-to-home

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,29 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import products from "../../products.json";
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" className="product-img" />
       </Link>
-
       <Card.Body>
         <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-
         <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
         </Card.Text>
-
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
