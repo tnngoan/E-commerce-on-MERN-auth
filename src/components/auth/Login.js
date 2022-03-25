eebca2383555b46e5f77d6a64e54bbed289b100f
tnngoan <ngoan.n.tr@gmail.com>
2022-03-26 05:41:19 +0700

    add ui for login screen

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useNavigate, useLocation } from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import FormContainer from "./FormContainer";
+import LoadingSpinner from "../layout/LoadingSpinner";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = () => {
+    console.log("submit");
+  };
 
-export default Login
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email" className="my-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password" className="my-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary" className="my-3">
+          Sign In
+        </Button>
+      </Form>
+      <Row className="py-3">
+        <Col>
+          New Customer ?{" "}
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default Login;
